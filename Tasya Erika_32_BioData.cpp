f7339334aad343704bc5f15782d8aedee73aa0ae
ibayyy <110375723+ibayyy@users.noreply.github.com>
2022-08-08 06:17:10 -0700

    Add files via upload

---
+++ Tasya Erika_32_BioData.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+system("color A");
+printf("\tBIODATA SISWA\n\n");
+printf("nama    :Tasya Erika\n");
+printf("kelas   :X-RPL2\n");
+printf("absen   :32\n");
+printf("hobi    :Berenang");
+getch();
+}
