ced024554a6d8bdd7fe505b4e0538f261d2cc957
RafiAthariandoPutra <110374803+RafiAthariandoPutra@users.noreply.github.com>
2022-08-08 20:07:10 +0700

    Add files via upload

---
+++ rafi_28_biodata.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+int main()
+{
+
+
+
+system ("color 017");
+printf ("====================================\n");
+printf ("BIODATA PRIBADI\n");
+
+printf ("nama      : Rafi athariando\n");
+printf ("absen     : 28\n");
+printf ("kelas     : X PPLG 2\n");
+printf ("hobi      : bermain gim dan nonton NBA\n");
+printf ("====================================\n");
+printf ("alamat    : jl.masjid al munir no.31\n");
+
+}
