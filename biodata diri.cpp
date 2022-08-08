3b63341c1a3e65c1d94040b394f3b2dc7db2d74f
RiskiDwiSaputra <110375685+RiskiDwiSaputra@users.noreply.github.com>
2022-08-08 15:09:11 +0700

    Add files via upload

---
+++ biodata diri.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+
+
+system("color B");
+printf("\tBIODATA SISWA\n\n");
+printf("nama     :riski dwi saputra\n");
+printf("kelas    :x rpl-2\n");
+printf("hobi     :playing game\n");
+printf("absen    :30\n");
+getch();
+}
