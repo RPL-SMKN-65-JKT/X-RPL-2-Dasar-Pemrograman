25d56390542e7704eb8147e7414143df9cf3a2cc
MuhamadZafarSyah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2022-08-08 06:21:43 -0700

    Add files via upload

---
+++ Muhamad Zafar Syah_22_BioData.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+system("color A");
+printf("\tBIODATA SISWA\n\n");
+printf("nama    :Muhamad Zafar Syah\n");
+printf("kelas   :X-RPL2\n");
+printf("absen   :22\n");
+printf("hobi    :Bermain Game");
+getch();
+}
