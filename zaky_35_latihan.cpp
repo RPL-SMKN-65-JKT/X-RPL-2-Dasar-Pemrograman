5fd91e3ae5b217a07b1e7286bc90dc1ad389e3c1
zakyyasar <110375122+zakyyasar@users.noreply.github.com>
2022-08-09 20:31:43 +0700

    Add files via upload

---
+++ zaky_35_latihan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i=10;
+
+   if (i==10)
+   {
+
+      if (i< 15)
+          cout<<"i is smaller then 15\n";
+
+      if (i < 12)
+          cout<<"i is smaller than 12 too\n";
+      else
+          cout<<"i is greater than is";
+   }
+    return 0;
+}
+
