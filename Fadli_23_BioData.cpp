efff13a2999bdb6b0fbc505e5d4e62dfa9748a3d
mhmmdfadliramadhan <110376126+mhmmdfadliramadhan@users.noreply.github.com>
2022-08-09 13:00:10 +0700

    Add files via upload

---
+++ Fadli_23_BioData.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+int main()
+{
+    system ("color 034");
+    printf ("\tBIODATA SISWA\n\n");
+   printf ("=================================\n");
+    printf ("nama     : M.Fadli ramadhan\n");
+    printf ("absen    : 23\n");
+    printf ("kelas    : X_rpl_2\n");
+    printf ("hobi     : bermain gim\n");
+
+    getch();
+
+}
