5c65d547e8b37b8dd4b5fb963362429344aa0fb3
nazwa aliya hidayah X RPL 2 <110377156+zwaalyh@users.noreply.github.com>
2022-08-15 14:44:59 +0700

    Add files via upload

---
+++ nazwa_26_latihan.cpp
@@ -0,0 +1,25 @@
+//C++ program to illustrate nested-if statement
+ #include <iostream>
+ using namespace std;
+
+int main()
+ {
+    int i = 10;
+
+    if (i == 10)
+    {
+        // First if statement
+        if (i < 15)
+           cout<<"i is smaller than 15\n";
+
+        // Nested - if statement
+        // Will only be executed if statement above
+        // is true
+        if (i < 12)
+            cout<<"i is smaller than 12 too\n";
+        else
+            cout<<"i is greater than 15";
+    }
+    return 0;
+}
+
