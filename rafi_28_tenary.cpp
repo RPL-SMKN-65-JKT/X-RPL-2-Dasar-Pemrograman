7c8cff979381b9c5d33c628e39510c65c5d068ff
RafiAthariandoPutra <110374803+RafiAthariandoPutra@users.noreply.github.com>
2022-08-15 14:24:45 +0700

    Add files via upload

---
+++ rafi_28_tenary.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main(){
+
+ int jawaban;
+ cout <<"berapakah hasil dari 3+4?" << endl;
+ cout <<"jawab>" ;
+ cin  >>jawaban;
+
+ string hasil = (jawaban == 7) ? "benar" : "salah";
+  cout <<"jawaban anda: " << hasil << endl;
+  return 0;
+  }
