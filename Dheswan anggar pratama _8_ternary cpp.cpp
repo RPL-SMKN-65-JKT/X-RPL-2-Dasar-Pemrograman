b8f46142b90c76fee95ca937a94c901a5b32de12
Dheswan1 <110374176+Dheswan1@users.noreply.github.com>
2022-08-15 14:14:50 +0700

    Add files via upload

---
+++ Dheswan anggar pratama _8_ternary cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+    int jawaban;
+    
+    cout << "Berapakah hasil 3+4?" << endl;
+    cout << "jawab> ";
+    cin >> jawaban;
+    
+    string hasil = (jawaban == 7) ? "benar" : "salah";
+    
+    cout << "jawaban anda: " << hasil << endl;
+    
+    return 0;
+    }
