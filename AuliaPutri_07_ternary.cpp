fbf5fc32f1e864295288cfbd6ba441c298ae2334
auliaputri2705 <110374425+auliaputri2705@users.noreply.github.com>
2022-08-15 14:30:57 +0700

    Add files via upload

---
+++ AuliaPutri_07_ternary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        int jawaban;
+
+            cout << "Berapakah hasil  3+4?" << endl;
+            cout << "jawab> ";
+            cin >> jawaban;
+
+            string hasil = (jawaban == 7) ? "Benar" : "Salah";
+
+            cout << "Jawaban anda: " << hasil << endl;
+
+            return 0;
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
