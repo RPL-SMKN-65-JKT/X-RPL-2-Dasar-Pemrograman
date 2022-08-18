d0f9bb6f486e923eff3114f9ac1aa457083181a3
auliaputri2705 <110374425+auliaputri2705@users.noreply.github.com>
2022-08-18 19:40:35 +0700

    Add files via upload

---
+++ AuliaPutriSakinah_07_Conditional.cpp
@@ -0,0 +1,321 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nama;
+
+    cout << "Masukkan Nama : Aulia" << endl;
+
+    int nilai;
+    string nlai;
+
+    cout << "Masukkan Nilai : " << endl;
+    cin >> nilai;
+
+    switch(nilai){
+        case 100 :
+           nlai = "A dan dinyatakan lulus";
+           break;
+        case 99 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 98 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 97 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 96 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 95 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 94 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 93 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 92 :
+            nlai = "A dan dinyatakan lulus";
+        case 91 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 90 :
+           nlai = "A dan dinyatakan  lulus";
+           break;
+        case 89 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 88 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 87 :
+            nlai = "A dan dianyattakan lulus";
+            break;
+        case 86 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 85 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 84 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 83 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 82 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 81 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 80 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 79 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 78 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 77 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 76 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 75 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 74 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 73 :
+            nlai = "A dan dinyatakan lulus";
+            break;
+        case 72 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 71 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 70 :
+            nlai = "B dan dinyatakan lulus";
+            break;
+        case 69 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 68 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 67 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 66 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 65 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 64 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 63 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 62 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 61 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 60 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 59 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 58 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 57 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 56 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 55 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 54 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 53 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 52 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 51 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 50 :
+            nlai = "C dan dinyatakan lulus";
+            break;
+        case 49 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 48 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 47 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 46 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 45 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 44 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 43 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 42 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 41 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 40 :
+            nlai = "D dan dinyatakan tidak lulus";
+            break;
+        case 39 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 38 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 37 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 36 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 35 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 34 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 33 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 32 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 31 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 30 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 29 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 28 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 27 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 26 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 25 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 24 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 23 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 22 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 21 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 20 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 19 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 18 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 17 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 16 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 15 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 14 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 13 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 12 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 11 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 10 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 9 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 8 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 7 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 6 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 5 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 4 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 3 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 2 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 1 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+        case 0 :
+            nlai = "E dan dinyatakan tidak lulus";
+            break;
+
+    }
+    cout << "Aulia mendapat nilai " << nlai;
+}
