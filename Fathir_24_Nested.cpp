e632da417585f3087f0a3b5b2e95527d89069428
MuhammadFathirRamadhan <110374686+MuhammadFathirRamadhan@users.noreply.github.com>
2022-08-22 14:54:11 +0700

    Add files via upload

---
+++ Fathir_24_Nested.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int i = 10;
+    if (i == 10)
+    {
+        if ( i < 15)
+            cout << " i is smaller than 15" << endl;
+        if  ( i < 12)
+            cout << "i is smaller than 12 too" << endl;
+        else
+            cout << "i is greater than 15";s
+
+    }
+}
