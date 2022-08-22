5c9f7ec6f138f9d9a82dd3325aa15a8dd9f69516
mhmmdfadliramadhan <110376126+mhmmdfadliramadhan@users.noreply.github.com>
2022-08-22 14:58:37 +0700

    Add files via upload

---
+++ Muhammad Fadli Ramadhan_23_Nested.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int i = 10;
+    if (i == 10)
+    {
+    if (i < 15)
+        cout << "i is smaller than 15";
+    if (i < 12)
+        cout << "i is smaller than 12 too";
+    else 
+        cout << "i is greater than 15";
+    }
+      
+}
