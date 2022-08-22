55a09a974048f3b9d36f44ec1e5f116148f57839
Dheswan1 <110374176+Dheswan1@users.noreply.github.com>
2022-08-22 14:56:51 +0700

    Add files via upload

---
+++ Dheswan anggar pratama_10_Nested.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int i = 10;
+    if (i == 10)
+    { 
+      if (i < 15)
+         cout<<"i is smaller than 15\n";
+     
+      if (i < 12)
+         cout<<"i is smaller than 12 too\n";
+      else
+         cout<<"i is greater than 15";
+         }
+         
+return 0;
+
+}         
