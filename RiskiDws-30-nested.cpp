e510f0199c75a24ef5ed0fbd9a059bd92b0e0432
RiskiDwiSaputra <110375685+RiskiDwiSaputra@users.noreply.github.com>
2022-08-22 14:50:27 +0700

    Add files via upload

---
+++ RiskiDws-30-nested.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int i = 10;
+   
+   if (i == 10)
+   {
+     
+     if (i < 15)
+        cout<<"i is smaller than 15/n";
+       
+       
+      
+      
+     if (i < 12)
+         cout<<"i is smaller than 12/n";
+     else
+         cout<<"i is greater than 15/n";
+   }
+   
+   return 0;
+}
