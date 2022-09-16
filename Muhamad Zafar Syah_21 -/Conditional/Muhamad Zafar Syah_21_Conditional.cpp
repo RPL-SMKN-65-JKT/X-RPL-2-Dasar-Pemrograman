46fb0686cbc4327fb514c846ed0d9a326aa179b9
MuhamadZafarSyah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2022-09-16 07:00:55 -0700

    TUGASNYA KUMPULIN MASZEH

---
+++ Muhamad Zafar Syah_21 -/Conditional/Muhamad Zafar Syah_21_Conditional.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int nilai;
+  string nama;
+  cout <<"PROGRAM NILAI SISWA DAN PERNYATAAN LULUS ATAU TIDAK LULUS\n"<<endl;
+
+  cout <<"Masukan nama anda  : \n";
+
+  cin >>nama;
+
+  cout <<"Masukan nilai anda  : \n";
+
+  cin>>nilai;
+
+  if(nilai >= 90)
+  {
+      cout<<"\n\nSelamat anda mendapat Nilai A dan dinyatakan LULUS "<<endl;
+  }
+  else if (nilai >= 70)
+  {
+     cout<<"\n\nSelamat anda mendapat Nilai B dan dinyatakan LULUS "<<endl;
+  }
+  else if (nilai >= 50)
+  {
+     cout<<"\n\nSelamat anda mendapat Nilai C dan dinyatakan LULUS\n\n"<<endl;
+  }
+  else if (nilai >= 40)
+  {
+      cout<<"\n\nMaaf anda mendapat Nilai D dan dinyatakan TIDAK LULUS\n\n"<<endl;
+  }
+  else if (nilai <= 40)
+  {
+      cout<<"n\nMaaf anda mendapat Nilai E dan dinyatakan TIDAK LULUS\n\n"<<endl;
+  }
+  return 0;
+  }
+
+
