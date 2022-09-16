df8cd0d555778f3d6ea9d0a2245528ab0dbb708a
RafiAthariandoPutra <110374803+RafiAthariandoPutra@users.noreply.github.com>
2022-09-16 22:01:45 +0700

    Add files via upload
    butuh ekskul valorant esport :)

---
+++ Rafi Athariando_27/rafi_28_conditional.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+using namespace std ;
+ int main() {
+
+ string nama ;
+ int nilai ;
+
+  cout <<"masukkan nama" << endl ;
+  cin  >> nama;
+  printf ("===========================\n");
+  cout <<"masukkan nilai"  << endl ;
+  cin  >> nilai ;
+  cout <<"hasil input nilai siswa" << endl;
+  printf ("===========================\n");
+
+
+if (nilai >90)
+{
+    cout <<"selamat! anda dapat nilai terbaik grade A dan dinyatakan lulus" << endl;
+}
+else if (nilai >80)
+{
+    cout <<"anda dapat grade B dan dinyatakan lulus" << endl;
+}
+else if (nilai >70)
+{
+    cout <<"anda dapat grade C  dan dinyatakan lulus" << endl;
+}
+else if (nilai >60)
+{
+    cout <<"sangat disayangkan, anda dapat grade C. TIDAK LULUS" << endl;
+}
+else if (nilai >50)
+{
+    cout <<"sangat disayangkan, anda dapat grade D.TIDAK LULUS" << endl;
+}
+else if (nilai <50)
+{
+    cout <<"ngulang lagi dah,luwh dapet grade D.TIDAK LULUS" << endl;
+}
+
+
+}
+
+
+
+
---
+++ Rafi Athariando_27/rafi_28_nested.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 20;
+    if (i == 20 )
+    {
+        if (i < 15)
+            cout <<"i is smaller than 15\n";
+        if (i < 12)
+            cout <<"i is smaller than 12 too\n";
+        else if (i > 15)
+            cout <<" i is greater than 15";
+    }
+}
