dd5a09efde413d39828817fbd42708950769629b
MuhamadZafarSyah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2022-09-26 06:46:01 -0700

    DI PUCUK DINGINN CUUYY
    muehehehe

---
+++ Muhamad Zafar Syah_21 -/Persegi Panjang/Muhamad Zafar Syah_21_Persegi Panjang.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    system("color 009");
+    char nomormenu;
+    float panjang,lebar;
+    float luas,keliling;
+    float diagonal;
+
+
+    cout<<" Menu Persegi Panjang"<<endl;
+    cout<<"=========================="<<endl;
+    cout<<" 1.Hitung Luas "<<endl;
+    cout<<" 2.Hitung Keliling "<<endl;
+    cout<<" 3.Hitung Panjang Diagonal "<<endl;
+    cout<<" 4.Keluar Program "<<endl;
+    cout<<"=========================="<<endl;
+    cout<<" Masukkan Pilihan Anda "<<endl;
+    cout<<"=========================="<<endl;
+    cin>>nomormenu;
+
+
+
+    switch (toupper(nomormenu)){
+        case '1':
+            {
+
+
+    cout<<"=========================="<<endl;
+    cout<<" Masukkan nilai panjang = ";
+    cin>>panjang;
+    cout<<" Masukkan nilai lebar = ";
+    cin>>lebar;
+    cout <<" Luas persegi panjang = ";
+    cout << (panjang * lebar);
+      break;
+    }
+        case '2':
+            {
+
+
+    cout<<"=========================="<<endl;
+    cout<<" Masukkan nilai panjang = ";
+    cin>>panjang;
+    cout<<" Masukkan nilai lebar = ";
+    cin>>lebar;
+    cout<<" Kelliling persegi panjang = ";
+    cout<< (2*panjang + 2*lebar);
+      break;
+            }
+          case '3' :
+            {
+             cout<<"=========================="<<endl;
+             cout<<" Masukkan nilai panjang = ";
+             cin>>panjang;
+             cout<<" Masukkan nilai lebar = ";
+             cin>>lebar;
+             diagonal=sqrt((double)panjang*panjang+(double)lebar*lebar);
+             cout<<"  Diagonal : "<<diagonal<<endl;
+             break;
+            }
+
+          case '4' :
+            {
+
+                cout<<"\nkeluar program\n";
+            }
+
+
+
+
+
+}
+
+}
+
+
+
