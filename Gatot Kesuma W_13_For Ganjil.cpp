3da0ea717684581a370a36a4cd6416679c8f19f4
Gatot Kesuma Wardana <110375181+Gatotkw@users.noreply.github.com>
2023-01-17 18:14:52 +0700

    Add files via upload

---
+++ Gatot Kesuma W_13_For Ganjil.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 10; i++) {
+        if(i % 2 != 0) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
