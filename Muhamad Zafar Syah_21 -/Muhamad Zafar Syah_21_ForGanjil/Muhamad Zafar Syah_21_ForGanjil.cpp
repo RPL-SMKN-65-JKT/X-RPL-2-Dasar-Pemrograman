8d9123e99a0a616b993fc45097c42ff197aef712
MuhamadZafarSyah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2023-01-17 01:55:55 -0800

    copas=neraka

---
+++ Muhamad Zafar Syah_21 -/Muhamad Zafar Syah_21_ForGanjil/Muhamad Zafar Syah_21_ForGanjil.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//PUNYA MUHAMMAD ZAFAR SYAH
+//YANG MALING INYAALLAH MASUK NERAKA
+int main() {
+    int a;
+    for(a = 1;a <= 50;a++) {
+        cout<<(a * 2) -1 << " ";
+    }
+    return 0;
+}
