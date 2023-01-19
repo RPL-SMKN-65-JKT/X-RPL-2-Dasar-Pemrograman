f2b08f1a3c00683c5b475bad82661b6988d3379c
MuhammadFathirRamadhan <110374686+MuhammadFathirRamadhan@users.noreply.github.com>
2023-01-19 19:20:30 +0700

    Add files via upload

---
+++ Fathir_24-tugas/MFathirR_25_ForGanjil/Fathir_25_Forganjil.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"Tugas Perulangan Angka Ganjil\n";
+    cout<<"Muhammad Fathir Ramadhan-25-PPLG 2\n\n";
+    system("color a");
+    for (i = 1; i < 51; i++){
+        cout << (i * 2) -1 << " ";
+    }
+
+    return 0;
+}
