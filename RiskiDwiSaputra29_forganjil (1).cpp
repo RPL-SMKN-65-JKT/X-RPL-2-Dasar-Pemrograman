4f6dcc71803d910f5305911e82c53bae06d4c393
RiskiDwiSaputra <110375685+RiskiDwiSaputra@users.noreply.github.com>
2023-01-19 18:29:13 +0700

    Add files via upload

---
+++ RiskiDwiSaputra29_forganjil (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int i;
+    for (i = 1 ; i< 51; i++) {
+    cout << (i * 2) -1 << " " ;
+    }
+    return 0;
+}
