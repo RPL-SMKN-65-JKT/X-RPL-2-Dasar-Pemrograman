bde58263b697c60825c691af7a3a00ce013b7836
MuhammadFathirRamadhan <110374686+MuhammadFathirRamadhan@users.noreply.github.com>
2023-01-24 14:42:51 +0700

    Add files via upload

---
+++ Fathir_24-tugas/Fathir_25_Helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    cin>>n;
+    i=1;
+    while (i<=n)
+    {
+    cout<<"hello world"<<endl;
+    i = i + 1;
+    }
+    return 0;
+}
