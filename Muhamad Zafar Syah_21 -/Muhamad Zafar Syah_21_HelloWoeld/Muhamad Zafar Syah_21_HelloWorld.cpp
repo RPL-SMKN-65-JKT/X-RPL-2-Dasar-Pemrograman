9ce1a8a6f321f0424b77f6d7212c74c7e0a00e65
MuhamadZafarSyah <110374556+MuhamadZafarSyah@users.noreply.github.com>
2023-01-23 23:42:33 -0800

    LMAO PISAN KALIAN KANG

---
+++ Muhamad Zafar Syah_21 -/Muhamad Zafar Syah_21_HelloWoeld/Muhamad Zafar Syah_21_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    cin >>n;
+      i = 1;
+    while (i <= n)
+    {
+    cout<<"hello world"<<endl;
+     i = i + 1;
+    }
+    return 0;
+
+}
