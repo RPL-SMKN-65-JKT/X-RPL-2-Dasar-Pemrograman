c7a66e830cd2c31184328680bb502ff2a176d03f
auliaputri2705 <110374425+auliaputri2705@users.noreply.github.com>
2023-01-28 18:37:43 +0700

    Add files via upload

---
+++ AuliaPutri_07_HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    cin>>n;
+    i=1;
+    while (1<=n)
+    {
+    cout<<"hello world"<<endl;
+    i = i + 1;
+    }
+    return 0;
+}
