f0e500dfb238c252e6c00bde3039d2b821644c2c
RiskiDwiSaputra <110375685+RiskiDwiSaputra@users.noreply.github.com>
2023-01-31 14:03:40 +0700

    Add files via upload

---
+++ riski dwi saputra tnggal 27_31.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  int i;
+  int n;
+  cin>>n;
+  i=1;
+  while (i<=n)
+    { 
+    cout << "Hello, Dcoder!";
+    i = i + 1;
+    }
+    return 0;
+  }
