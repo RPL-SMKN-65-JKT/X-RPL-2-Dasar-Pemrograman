d10c7729f37b87a29419d21f286b6bf41da4d0f8
alzahra <123425215+Alxahra@users.noreply.github.com>
2023-01-31 14:43:35 +0700

    Add files via upload

---
+++ Alzahrayuslia_4_forganjil.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//compiler veesion g++ 6.3.0
+
+int main()
+{
+    int i;
+    for (i = 1 ; i< 49; i++) {
+   cout << (i* 2) -1 << " " ;
+}
+ return 0;
+ }
